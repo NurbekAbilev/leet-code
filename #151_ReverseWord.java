d7a7ca220e9161df2e03762bc5e0fe4a3afbc3e7
NurbekAbilev <38177308+NurbekAbilev@users.noreply.github.com>
2018-07-13 14:58:12 +0600

    Create #151_ReverseWord.java

---
+++ #151_ReverseWord.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public String reverseWords(String s) {
+          java.util.Scanner sc = new Scanner(s);
+        ArrayList<String> words = new ArrayList<>();
+        boolean d = false;
+        while(sc.hasNext()){
+            words.add(sc.next());
+            d = true;
+        }
+        String sent = "";
+        for(int i=words.size()-1;i>=0;i--){
+            sent+=words.get(i)+" ";
+        }
+        if(d)
+        return sent.substring(0,sent.length()-1);
+        else return "";
+        
+    }
+}
